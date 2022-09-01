650914b0e325af8a5c200d3ace7867314cd93c08
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2022-09-01 23:01:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Description
+
+Simple KERNEL for Atari 2600
+
+To compile use DASM, and to run it use Stella emulator. This can be achieved with the following command under Linux
+
+```
+$ dasm first.asm -ocart.bin -f3 && stella cart.bin
+```
+
+## References
+
+1. [DASM Homepage](https://dasm-assembler.github.io/)
+2. [Atari 2600 Assembly for newbies](https://www.randomterrain.com/atari-2600-memories-tutorial-andrew-davie-01.html)
+3. [AtariAge 2600 Programming Resources](https://www.atariage.com/2600/programming/index.html)
---
+++ first.asm
@@ -0,0 +1,62 @@
+        processor 6502
+
+        include "vcs.h"
+        include "macro.h"
+
+        SEG
+        ORG $F000
+
+reset
+fstart
+	;; Start of vertical blank processing
+        lda #0
+        sta VBLANK
+
+        lda #2
+        sta VSYNC
+
+        ;; 3 scanlines of VSYNCH signal...
+        sta WSYNC
+        sta WSYNC
+        sta WSYNC
+
+        lda #0
+        sta VSYNC           
+
+        ;; 37 scanlines of vertical blank...
+	;; =====================================
+	REPEAT 37
+        sta WSYNC
+	REPEND
+	
+	;; 192 scanlines of picture...
+	;; =====================================
+        ldx #0
+        REPEAT 192; scanlines
+
+        inx
+        stx COLUBK
+
+	sta WSYNC
+
+        REPEND
+
+        lda #%01000010
+        sta VBLANK                     ; end of screen - enter blanking
+	
+	;; 30 scanlines of overscan...
+	;; =====================================
+	REPEAT 30
+        sta WSYNC
+	REPEND
+
+        jmp fstart
+
+	;; RESET & INTERRUPT VECTORS
+	;; =====================================	
+        ORG $FFFA
+        .word reset          ; NMI
+        .word reset          ; RESET
+        .word reset          ; IRQ
+
+    	END
